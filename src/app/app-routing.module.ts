c826ba777bbdc627ee82d1bbb5090c05b65a9073
Hannah <kimbumisso@gmail.com>
2018-11-28 14:21:17 +0100

    Rename page components and add basic routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainPage } from './pages/main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'list',
+    pathMatch: 'full'
+  },
+  {
+    path: 'list',
+    component: MainPage,
+    data: {
+      title: 'Tenant List',
+    },
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
