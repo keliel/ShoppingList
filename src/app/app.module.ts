4829acc2fa343c7639f8dee8a012a8de153ea8d8
Hannah Schinz <kimbumisso@gmail.com>
2018-11-30 10:15:37 +0100

    Extract navigation to own component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,11 +8,13 @@ import { AppComponent } from './app.component';
 import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { MainPage } from './pages/main-page/main-page.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainPage
+    MainPage,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
