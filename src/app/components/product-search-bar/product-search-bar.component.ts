143d4bc287a918df30cfab8b056a451852cb8403
Hannah Schinz <kimbumisso@gmail.com>
2018-11-30 08:47:08 +0100

    Implemented search bar on default page for easy testing

--- src/app/components/product-search-bar/product-search-bar.component.ts
+++ src/app/components/product-search-bar/product-search-bar.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Observable } from 'rxjs';
-import {debounceTime, distinctUntilChanged, map} from 'rxjs/operators';
-
-const states = [];
+import { debounceTime, distinctUntilChanged, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-product-search-bar',
@@ -10,24 +8,27 @@ const states = [];
   styleUrls: ['./product-search-bar.component.scss']
 })
 
-export class ProductSearchBarComponent implements OnInit {
+export class ProductSearchBarComponent implements OnInit, OnDestroy {
 
   public model: any;
 
+  states = [];
+
   constructor() { }
-  
+
   search = (text$: Observable<string>) =>
-  text$.pipe(
-    debounceTime(200),
-    distinctUntilChanged(),
-    map(term => term.length < 2 ? []
-      : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
-  )
+    text$.pipe(
+      debounceTime(200),
+      distinctUntilChanged(),
+      map(term => term.length < 1 ? []
+        : this.states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1).slice(0, 10))
+    )
 
   ngOnInit() {
   }
 
   ngOnDestroy(): void {
+    console.log('Nothing to destroy');
   }
 
 }
