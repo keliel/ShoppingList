472af5d9b11015bdc4ea63ea8a6c77e819b66662
Hannah Schinz <kimbumisso@gmail.com>
2018-11-30 19:33:04 +0100

    Some more minor enhancements

--- src/app/shared/services/product.service.ts
+++ src/app/shared/services/product.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { ProductSearchResult } from '../models/product-search-result';
 import { map } from 'rxjs/operators';
